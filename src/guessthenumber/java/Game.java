0856adc80377b411a2875a67ad4ab31c4227b805
martineng <martin.eca.6651@gmail.com>
2017-11-27 15:53:58 +0800

    Completed Game.java

--- src/guessthenumber/java/Game.java
+++ src/guessthenumber/java/Game.java
@@ -2,7 +2,79 @@
  */
 package guessthenumber.java;
 
+import java.util.Random;
+
 public class Game 
 {
+    private int [] numberArray;
+    private int empty1, empty2, empty3;
     
+    // Null Constructor
+    public Game()
+    {
+        initializeNumberArray();
+        initializeEmptySlots();
+    }
+    
+    // Setter
+    public void setEmpty1(int inEmpty)
+    {
+        empty1 = inEmpty;
+    }
+    
+    public void setEmpty2(int inEmpty)
+    {
+        empty2 = inEmpty;
+    }
+    
+    public void setEmpty3(int inEmpty)
+    {
+        empty3 = inEmpty;
+    }
+    
+    // Getter
+    public int getEmpty1()
+    {
+        return empty1;
+    }
+    
+    public int getEmpty2()
+    {
+        return empty2;
+    }
+    
+    public int getEmpty3()
+    {
+        return empty3;
+    }
+    
+    public int getNumberArrayValue(int inIndex)
+    {
+        return numberArray[inIndex];
+    }
+    
+    // Generate a random number between 0 to inRange
+    private int generateRandomNumber(int inRange)
+    {
+        Random rand = new Random();
+        
+        return rand.nextInt(inRange);
+    }
+    
+    private void initializeNumberArray()
+    {
+        numberArray = new int[10];
+        
+        for (int index = 0; index < 10; index++)
+        {
+            numberArray[index] = generateRandomNumber(98) + 1; // Eliminate 0
+        } // END FOR
+    }
+    
+    private void initializeEmptySlots()
+    {
+        setEmpty1(generateRandomNumber(2));
+        setEmpty2(3);
+        setEmpty3(generateRandomNumber(4)+4);
+    }
 }
